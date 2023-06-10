baa1441e23bc9498e1d85d63edf027f20e192216
TiagoFreitas03 <tiago.freitas.faria@gmail.com>
2023-06-09 22:53:28 -0300

    rota de resumo dos hábitos

---
+++ server/src/controllers/SummaryController.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from 'express'
+import { z } from 'zod'
+import dayjs from 'dayjs'
+
+import { connection } from '../database/connection'
+
+export class SummaryController {
+	async index(req: Request, res: Response) {
+		const getSummaryParams = z.object({
+			year: z.coerce.number({ required_error: 'Informe o ano' }),
+			month: z.coerce.number({ required_error: 'Informe o mês' })
+				.min(1, 'Mês inválido').max(12, 'Mês inválido')
+		})
+
+		const { year, month } = getSummaryParams.parse(req.params)
+
+		const monthStart = new Date(year, month - 1, 1, 0, 0, 0, 0)
+		const monthEnd = dayjs(monthStart).endOf('month').toDate()
+
+		const { user_id } = req
+
+		const user = await connection.user.findFirst({
+			where: {
+				id: user_id,
+				created_at: { lte: monthEnd }
+			}
+		})
+
+		if (!user) {
+			return res.status(404).json({ message: 'Usuário não encontrado' })
+		}
+
+		const summary = await connection.$queryRaw`
+      SELECT
+				d.id,
+				d.date,
+				(
+					SELECT CAST(COUNT(*) AS FLOAT)
+					FROM completed_habits c
+					JOIN habits h ON h.id = c.habit_id
+					WHERE c.day_id = d.id
+						AND h.user_id = ${user_id}
+				) AS completed,
+				(
+					SELECT CAST(COUNT(*) AS FLOAT)
+					FROM habit_week_days hwd
+					JOIN habits h ON h.id = hwd.habit_id
+					WHERE hwd.week_day = CAST(strftime('%w', d.date / 1000.0, 'unixepoch') AS INT)
+						AND h.created_at <= d.date
+						AND h.user_id = ${user_id}
+				) AS amount
+			FROM days d
+			WHERE d.date BETWEEN ${monthStart.getTime()} AND ${monthEnd.getTime()}
+    `
+
+		return res.json(summary)
+	}
+}
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,12 +4,14 @@ import { verifyJwt } from './middlewares/auth'
 import { UsersController } from './controllers/UsersController'
 import { HabitsController } from './controllers/HabitsController'
 import { DaysController } from './controllers/DaysController'
+import { SummaryController } from './controllers/SummaryController'
 
 const routes = Router()
 
 const usersCtrl = new UsersController()
 const habitsCtrl = new HabitsController()
 const daysCtrl = new DaysController()
+const summaryCtrl = new SummaryController()
 
 routes.post('/users', usersCtrl.create)
 routes.post('/login', usersCtrl.login)
@@ -18,5 +20,7 @@ routes.post('/habits', verifyJwt, habitsCtrl.create)
 routes.patch('/habits/:id/toggle', verifyJwt, habitsCtrl.toggle)
 
 routes.get('/day/:date', verifyJwt, daysCtrl.show)
+
+routes.get('/summary/:year/:month', verifyJwt, summaryCtrl.index)
 
 export { routes }
